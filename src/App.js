7d7b67e038a2db21c6b7de7f5f7b3e71e9b58dff
indira1vik <indiravik113@gmail.com>
2023-04-25 23:27:47 +0530

    Added HashRouter

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import Detail from "./Detail";
 import Home from "./Home";
 import Splash from "./Splash";
 import './style/App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app-cont">
-      <BrowserRouter basename="/cinemize">
+      <HashRouter>
         <Routes>
-          <Route exact path="/cinemize" element={<Splash />} />
-          <Route path="/cinemize/home" element={<Home />} />
-          <Route path="/cinemize/detail" element={<Detail />} />
+          <Route exact path="/" element={<Splash />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
